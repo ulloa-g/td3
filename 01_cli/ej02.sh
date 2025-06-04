b66b4b7268684d5d92ae321dc08549baa055116d
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-06-04 00:46:29 -0300

    agrego comentario respecto a ./

--- 01_cli/ej02.sh
+++ 01_cli/ej02.sh
@@ -18,5 +18,15 @@ echo "Ejecutando el programa compilado:"
 ./hola-mundo
 
 # ¿porque es necesario "./" para ejecutar mi archivo?
-# porque el ejecutable está en el directorio actual
-# entonces es necesario prefijarlo con "./"
+
+# Explicación:
+# En Linux (Unix), es necesario ejecutar cualquier ejecutable con el prefijo ./
+# Cuando escribes un comando como ls, mkdir o gcc en la terminal, el sistema no sabe 
+# inmediatamente dónde encontrar el programa correspondiente. 
+# Para eso, utiliza una variable de entorno llamada PATH
+# la variable PATH contiene una lista de directorios donde el shell debe buscar los ejecutables.
+# El directorio actual (./) no está en el PATH por defecto por razones de seguridad
+# Garantiza que solo ejecutes programas desde tu directorio actual cuando explícitamente lo indicas
+# evitando la ejecución accidental de archivos maliciosos con nombres idénticos a comandos del sistema 
+# que puedan estar presentes en tu directorio de trabajo. 
+# Es una buena práctica que contribuye a la robustez y seguridad de tu sistema.
