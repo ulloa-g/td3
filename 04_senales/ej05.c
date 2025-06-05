c4e9359999ce59149effa3500b604e3c78723445
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-06-05 16:49:12 -0300

    explico funcion write()

--- 04_senales/ej05.c
+++ 04_senales/ej05.c
@@ -14,3 +14,13 @@ int main(){
 	
 	return 0;
 }
+
+/*
+Explicación de:
+ssize_t write(int fd, const void *buf, size_t count);
+write(STDOUT_FILENO, "Me rehuso a terminar\n", sizeof("Me rehuso a terminar\n"));
+
+write() es una llamada al sistema que escribe datos en un descriptor de archivo.
+STDOUT_FILENO: constante que representa el descriptor de archivo (fd) estándar de salida (stdout) = 1
+sizeof(): Calcula el tamaño en bytes a escribir
+*/
