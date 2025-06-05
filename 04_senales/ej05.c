be48e28dd4bd44b073371f9237d8224ef113b265
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-06-05 16:43:11 -0300

    ej05 finalizado

---
+++ 04_senales/ej05.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void mi_manejador(){
+	write(STDOUT_FILENO, "Me rehuso a terminar\n", sizeof("Me rehuso a terminar\n"));
+}
+
+int main(){
+	signal(SIGUSR1, mi_manejador);  // SIGKILL no puede ser ignorada o manejada
+	printf("Soy el proceso con ID: %d\n", getpid());
+	while(1);
+	
+	return 0;
+}
