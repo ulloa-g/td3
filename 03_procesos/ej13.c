0d09b6495d8844744df4f42db957d48b1ff17660
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-06-05 14:42:14 -0300

    ej13 finalizado

---
+++ 03_procesos/ej13.c
@@ -0,0 +1,39 @@
+//TP Procesos, Ejercicio 13
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define PS_MACRO execl("/bin/sh", "sh", "-c", "ps -ef | grep proc_09", NULL);
+
+int main()
+{
+    pid_t pid, ppid;
+
+    pid = fork();
+
+    if (pid < 0) {
+        fprintf(stderr, "fork falló\n");
+        exit(1);
+        
+    } else if (pid == 0) { // proceso hijo
+        printf("Proceso hijo  (PID=%d) está corriendo.\n", getpid());
+        
+        sleep(5); 
+        
+        printf("Proceso hijo  (PID=%d) está saliendo.\n", getpid());
+
+        exit(0);
+    
+    } else { // proceso padre
+        while (1)
+        {
+            printf("Proceso padre (PID=%d) está corriendo.\n", getpid());
+            sleep(15); 
+            printf("Proceso padre (PID=%d) está saliendo.\n", getpid());
+        }
+    }
+    
+    return 0;
+}
