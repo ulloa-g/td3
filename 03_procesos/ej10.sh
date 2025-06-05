b08f311abbdbed2a86755c0f8e1e74ca2febfe22
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-06-05 12:14:58 -0300

    ej10 finalizado

---
+++ 03_procesos/ej10.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILE_NAME="ej10.c"
+
+echo "Compilando " $FILE_NAME
+gcc ej10.c -o ej10
+
+echo "Ejecutando " $FILE_NAME
+./ej10
+
+echo "Programa finalizado."
+echo "Puede verificar el EXIT STATUS con el comando"
+
