4a36a84a9084098bc7235926711ab4d66b66d710
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-06-11 06:55:59 -0300

    ej04 finalizado

--- 05_pipe/ej04.c
+++ 05_pipe/ej04.c
@@ -20,36 +20,37 @@ void pipe_sign_handler(int a){
 
 int main (){
 
-   int ipc[2];
-   int leido, success;
+   int fd[2];
+   int leido;
    char buff[BUFF_SIZE] = {0};
 
    signal(SIGPIPE, pipe_sign_handler);
    
-   if (pipe(ipc) == -1)
+   if (pipe(fd) == -1)
       exit(-1);
    
-   close(ipc[0]);
-   
-   switch (fork()){ 
       
-      case 0:
-         close(ipc[1]);
-         printf("Hijo leyendo tuberia... \n");
-         leido = read(ipc[0], buff, sizeof(buff));
-         if(leido < 1){
-            write(STDOUT_FILENO, "Error al leer tuberia\n", sizeof("Error al leer tuberia\n"));
-         }else {
-            write(STDOUT_FILENO, "Leido de la tuberia \"", sizeof("Leido de la tuberia \""));
-            write(STDOUT_FILENO, buff, leido-1);
-            printf("\" por el proceso hijo.\n");
+      switch (fork()){ 
+          
+        case 0:
+            //close(fd[0]);
+            close(fd[1]);
+            printf("Hijo leyendo tuberia... \n");
+            leido = read(fd[0], buff, sizeof(buff));
+            if(leido < 1){
+                write(STDOUT_FILENO, "Error al leer tuberia\n", sizeof("Error al leer tuberia\n"));
+            }else {
+                write(STDOUT_FILENO, "Leido de la tuberia \"", sizeof("Leido de la tuberia \""));
+                write(STDOUT_FILENO, buff, leido-1);
+                printf("\" por el proceso hijo.\n");
          }
          exit(0);
          
-      default:
-         strncpy(buff, DATA, sizeof(DATA));
-         write(ipc[1], buff, sizeof(DATA));
-         wait(NULL);      
-         exit(0);      
+        default:
+            close(fd[0]);  // Cierra el extremo de lectura de la tubería en el padre
+            strncpy(buff, DATA, sizeof(DATA));
+            write(fd[1], buff, sizeof(DATA));
+            wait(NULL);      
+            exit(0);      
    }
 }
