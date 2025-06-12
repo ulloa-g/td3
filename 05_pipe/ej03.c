f2afde22168990682af9ddd46c25a51a2c515740
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-06-12 19:25:19 -0300

    algunos comentarios al ej03

--- 05_pipe/ej03.c
+++ 05_pipe/ej03.c
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <sys/wait.h> 
 
-#define FRASE_A "INFORMACION A"
+#define FRASE_A "HOLA"
 #define FRASE_B "INFORMACION IMPORTANTE B"
 #define BUFF_SIZE 80
 
@@ -19,15 +19,20 @@ int main (){
    int tamaño_frase;
    pipe(fd);
 
-   switch (fork()){ 
+   switch (fork()){
       
       case 0:
-	   close(fd[0]);
-       tamaño_frase = strlen(FRASE_A) + 1; // +1 para incluir el carácter nulo "\0" al final de la cadena
-	   write(fd[1], &tamaño_frase, sizeof(int));  // Escribe el tamaño de FRASE_A en la tubería
-       write(fd[1], FRASE_A, tamaño_frase);  // Escribe FRASE_A en la tubería
-	   close(fd[1]);
-	   exit(0);
+         close(fd[0]);
+         // tamaño_frase = 5
+         tamaño_frase = strlen(FRASE_A) + 1; // +1 para incluir el carácter nulo "\0" al final de la cadena
+         // &tamaño_frase es dirección de memoria, apunta a los 4KB que ocupa un entero (5 es un entero)
+         write(fd[1], &tamaño_frase, sizeof(int));  // Escribe el tamaño de FRASE_A en la tubería
+         write(fd[1], FRASE_A, tamaño_frase);  // Escribe FRASE_A en la tubería
+         close(fd[1]);
+         printf("Numero de caracteres en la FRASE_A: %d\n", tamaño_frase);  
+         
+         printf("Esto muestra &tamaño_frase: %p\n", &tamaño_frase);  
+         exit(0);
       break;
       
       default:
@@ -88,4 +93,12 @@ Nota:
 printf("bytes_leidos: %zd", bytes_leidos);  --------> 4
 printf("sizeof(): %zd", sizeof(bytes_leidos)); -----> 8
 Esto es porque bytes_leidos es de tipo ssize_t, que en sistemas de 64 bits ocupa 8 bytes, mientras que sizeof(int) es 4 bytes.
+
+Otras consideraciones:
+
+&tamaño_frase es la dirección de memoria de la variable tamaño_frase
+Si tamaño_frase vale 5, en memoria esa variable contiene el número 5 (en binario, usando 4 bytes si `int` es de 4 bytes).
+Supón que tamaño_frase = 5; y ocupa 4 bytes en memoria.  
+&tamaño_frase apunta al inicio de esos 4 bytes.
+write() toma esos 4 bytes y los escribe tal cual, no como texto, sino como datos binarios.
 */
